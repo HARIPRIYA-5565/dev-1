951f84a8c947ab0947598a41df89ffaf30c3b056
HARIPRIYA-5565 <70848158+HARIPRIYA-5565@users.noreply.github.com>
2025-03-05 12:40:40 +0000

    driverDashboard

---
+++ my-project/src/Components/driverDashboard.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+const driverList=[{
+    id: 0,
+    name: "Krishanku Borah",
+    idNo: "256512",
+    subscription: "Active Subscription",
+    regDate: "25/6/12",
+    totalRides: "111",
+    titleTR:"Total Rides",
+    clientCancellations:"11",
+    titleCC: "Client Cancellations",
+    driverCancellation: "1",
+    titleDC: "Driver Cancellation",
+},
+{
+    id: 1,
+    name: "Krishanku Borah",
+    idNo: "256512",
+    subscription: "Expired Cancellation",
+    regDate: "25/6/12",
+    totalRides: "111",
+    titleTR:"Total Rides",
+    clientCancellations:"11",
+    titleCC: "Client Cancellations",
+    driverCancellation: "1",
+    titleDC: "Driver Cancellation",
+},
+{
+    id: 2,
+    name: "Krishanku Borah",
+    idNo: "256512",
+    subscription: "Active Subscription",
+    regDate: "25/6/12",
+    totalRides: "111",
+    titleTR:"Total Rides",
+    clientCancellations:"11",
+    titleCC: "Client Cancellations",
+    driverCancellation: "1",
+    titleDC: "Driver Cancellation",
+},
+{
+    id: 3,
+    name: "Krishanku Borah",
+    idNo: "256512",
+    subscription: "Active Subscription",
+    regDate: "25/6/12",
+    totalRides: "111",
+    titleTR:"Total Rides",
+    clientCancellations:"11",
+    titleCC: "Client Cancellations",
+    driverCancellation: "1",
+    titleDC: "Driver Cancellation",
+},
+{
+    id: 4,
+    name: "Krishanku Borah",
+    idNo: "256512",
+    subscription: "Active Subscription",
+    regDate: "25/6/12",
+    totalRides: "111",
+    titleTR:"Total Rides",
+    clientCancellations:"11",
+    titleCC: "Client Cancellations",
+    driverCancellation: "1",
+    titleDC: "Driver Cancellation",
+},
+]
+
+export default function driverDashboard(){
+return(
+    <div>
+        {driverList.map((item)=>
+       
+)}
+</div>
+)
+}
